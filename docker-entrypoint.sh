3984f7d4d2e9eb631d17afc13e03a1578c53de6d
pitchy3 <mike@thevallones.com>
2026-05-25 20:47:48 -0700

    Fix entrypoint user fallback and argv forwarding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,31 +5,41 @@ PUID="${PUID:-1000}"
 PGID="${PGID:-1000}"
 
 if [ "$(id -u)" -eq 0 ]; then
+    target_group="app"
     if getent group app >/dev/null 2>&1; then
         current_gid="$(getent group app | cut -d: -f3)"
         if [ "$current_gid" != "$PGID" ]; then
             if getent group "$PGID" >/dev/null 2>&1; then
-                existing_group="$(getent group "$PGID" | cut -d: -f1)"
-                usermod -g "$existing_group" app
+                target_group="$(getent group "$PGID" | cut -d: -f1)"
+                usermod -g "$target_group" app
             else
                 groupmod -g "$PGID" app
             fi
         fi
-    elif ! getent group "$PGID" >/dev/null 2>&1; then
+    elif getent group "$PGID" >/dev/null 2>&1; then
+        target_group="$(getent group "$PGID" | cut -d: -f1)"
+    else
         groupadd -g "$PGID" app
     fi
 
     if getent passwd app >/dev/null 2>&1; then
         current_uid="$(id -u app)"
         if [ "$current_uid" != "$PUID" ]; then
-            usermod -u "$PUID" app
+            if getent passwd "$PUID" >/dev/null 2>&1; then
+                usermod -u "$PUID" -o app
+            else
+                usermod -u "$PUID" app
+            fi
         fi
-    elif ! getent passwd "$PUID" >/dev/null 2>&1; then
+        usermod -g "$target_group" app
+    elif getent passwd "$PUID" >/dev/null 2>&1; then
+        useradd --uid "$PUID" --gid "$PGID" --create-home --shell /usr/sbin/nologin -o app
+    else
         useradd --uid "$PUID" --gid "$PGID" --create-home --shell /usr/sbin/nologin app
     fi
 
     chown -R "$PUID:$PGID" /config
-    exec su app -s /bin/sh -c "$*"
+    exec su app -s /bin/sh -c 'exec "$@"' sh "$@"
 fi
 
 exec "$@"
